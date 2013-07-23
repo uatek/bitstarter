ada3b234896e8ff7864f23e028f5a59c6abfeb0e
uatek <rodriguezjoseangel@gmail.com>
2013-07-23 17:59:25 +0200

    Adds (non working) code to process the code from a URL.

--- grader.js
+++ grader.js
@@ -24,8 +24,17 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
+var assertUrlIsValid = function(url) {
+  var urlstring = url.toString();
+  
+  // TODO Really chech that the url is valid.
+  
+  return urlstring;
+};
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -40,12 +49,27 @@ var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+var cheerioHtmlUrl = function(htmlurl) {
+  return cheerio.load(rest.get(htmlurl));
+};
+
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
+    var checks = loadChecks(checksfile).sort();
+    var out = {};
+    for(var ii in checks) {
+        var present = $(checks[ii]).length > 0;
+        out[checks[ii]] = present;
+    }
+    return out;
+};
+
+var checkHtmlUrl = function(htmlurl, checksfile) {
+    $ = cheerioHtmlUrl(htmlurl);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -64,9 +88,22 @@ var clone = function(fn) {
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), "")
+	.option('-u, --url <url_to_html_file>', 'Url to index.html', clone(assertUrlIsValid), "")
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+	
+    var checkJson;
+    if (program.file != "" && program.url != "") {
+      console.log("Please, specify only one of --url or --file parameters.");
+      process.exit(1);
+    }
+    else if (program.url == "") {
+      program.file = HTMLFILE_DEFAULT;
+      checkJson = checkHtmlFile(program.file, program.checks);
+    }
+    else {
+      checkJson = checkHtmlUrl(program.url, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
